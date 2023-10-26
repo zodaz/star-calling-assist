a96817509cae3b86bcc67cbf92333e25d69d3a9c
Jonas Karlsson <jonas_k_t@hotmail.com>
2023-10-26 15:50:29 +0200

    Various UI related constants/enums

---
+++ src/main/java/com/starcallingassist/sidepanel/constants/OrderBy.java
@@ -0,0 +1,9 @@
+package com.starcallingassist.sidepanel.constants;
+
+public enum OrderBy
+{
+    WORLD,
+    TIER,
+    LOCATION,
+    DEAD_TIME,
+}
---
+++ src/main/java/com/starcallingassist/sidepanel/constants/RegionKeyName.java
@@ -0,0 +1,20 @@
+package com.starcallingassist.sidepanel.constants;
+
+public class RegionKeyName
+{
+    public static final String KEY_ASGARNIA = "asgarnia";
+    public static final String KEY_KARAMJA = "karamja";
+    public static final String KEY_FELDIP = "feldip";
+    public static final String KEY_FOSSIL = "fossil";
+    public static final String KEY_FREMMENIK = "fremmenik";
+    public static final String KEY_KOUREND = "kourend";
+    public static final String KEY_KANDARIN = "kandarin";
+    public static final String KEY_KEBOS = "kebos";
+    public static final String KEY_DESERT = "desert";
+    public static final String KEY_MISTHALIN = "misthalin";
+    public static final String KEY_MORYTANIA = "morytania";
+    public static final String KEY_GNOME = "gnome";
+    public static final String KEY_TIRANNWN = "tirannwn";
+    public static final String KEY_WILDERNESS = "wilderness";
+    public static final String KEY_UNKNOWN = "unknown";
+}
